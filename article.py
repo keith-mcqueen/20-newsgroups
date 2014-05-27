74c001c8d21ca3c0f7d116149be1e97e60f7386d
Eric Christopher Hullinger <ericch@boxing.cs.byu.edu>
2014-05-27 10:55:50 -0600

    Parsing the article

--- article.py
+++ article.py
@@ -3,9 +3,10 @@ import os
 
 def parse_article(filepath, filename, dictionary, stop_words=None):
     # load the file
-    file = open(filepath + os.sep + filename, 'r')
+    article_path = os.path.join(filepath, filename)
+    with open(os.path.join(filepath, filename), "r") as in_file:
+        for word in str.split(in_file.read()):
+            word = word.lower()
+            dictionary[word] = dictionary.setdefault(word, 0) + 1
 
-    # read the words
-    for line in file:
-        #print line
-        pass
+    print str(dictionary)
