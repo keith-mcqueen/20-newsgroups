3bc498f372fdd7c1262f380cce73b9cb16fb66dd
Eric Christopher Hullinger <ericch@cricket.cs.byu.edu>
2014-05-28 21:54:20 -0600

    Created framework for the models

---
+++ models.py
@@ -0,0 +1,17 @@
+import os
+import errno
+import math
+from category import Category
+ 
+ 
+def bernoulli(category, article_words):
+	
+	return 0
+
+def multinomial(category, article_words):
+	
+	return 0
+
+def baseline(category, article_words):
+	return category.prior_probability
+	
