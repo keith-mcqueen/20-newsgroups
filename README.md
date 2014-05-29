702c889824ffd0bd98a9bce60fdb260ff05c1fb3
Keith McQueen <keith.mcqueen@macweenies.com>
2014-05-28 22:50:09 -0600

    Added "multinomial" and "bernoulli" structure to the database.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 =============
 
 A Python implementation of a Naive-Bayes classifier applied to data from the 20 newsgroups data set.
+
+How to run:
+
+>>> python nb-classifier.py --docroot=/path/to/documents
+--database=/path/to/knowledge/base
+--stop-list=/path/to/file/with/stop/words
--- article.py
+++ article.py
@@ -4,7 +4,6 @@ import re
 
 def parse_article(filepath, filename, dictionary, stop_words=None):
     # load the file
-    article_path = os.path.join(filepath, filename)
     with open(os.path.join(filepath, filename), "r") as in_file:
         input_str = in_file.read()
         input_str = re.sub(r'\W+', ' ', input_str)
@@ -12,6 +11,8 @@ def parse_article(filepath, filename, dictionary, stop_words=None):
         for word in str.split(input_str):
             if word not in stop_words:
                 word = word.lower()
-                dictionary[word] = dictionary.setdefault(word, 0) + 1
+                dictionary[word] = dictionary.setdefault(word, {"multinomial": 0, "bernoulli": {}})
+                dictionary[word]["multinomial"] += 1
 
-    #print str(dictionary)
+                bernoulli = dictionary[word]["bernoulli"]
+                bernoulli[filename] = bernoulli.setdefault(filename, {filename: 1})
