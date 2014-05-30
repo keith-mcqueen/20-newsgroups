976a948b093ad121ebbbe112bfb307c254886eba
eric <eric@eric-VirtualBox.(none)>
2014-05-29 21:41:55 -0600

    Added a simple smoothing method where if the word does not exist then it will subtract 1/word_count from one and take the log of that to add (in this case it will be adding a negative) so that it lowers the probability that the document belongs to that category.

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ def bernoulli(category, article_words):
         if word in category.word_counts:
             word_probability = category.word_counts[word]["b"]
         else:
-            word_probability = 1
+            word_probability = 1 - 1/len(article_words)
 
         probability += math.log(word_probability)
 
@@ -23,7 +23,7 @@ def multinomial(category, article_words):
         if word in category.word_counts:
             word_probability = category.word_counts[word]["m"]
         else:
-            word_probability = 1
+            word_probability = 1 - 1/len(article_words)
 
         probability += math.log(word_probability)
 
